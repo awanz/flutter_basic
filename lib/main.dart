0d48560185980a0ada790c7b9180e37b2c890fe0
awan <me.awan.es@gmail.com>
2021-07-18 17:19:53 +0700

    button

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,52 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.yellow,
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Appbar"),
-          ),
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                child: Text("Hai"),
-                decoration: BoxDecoration(color: Colors.red),
-              ),
-              Container(
-                child: Text("Hai"),
-                decoration: BoxDecoration(color: Colors.yellow),
-              ),
-              Container(
-                child: Text("Hai"),
-                decoration: BoxDecoration(color: Colors.green),
-              ),
-              Column(
-                children: <Widget>[
-                  Text("Kolom"),
-                  Text('Lorem ipsum dolor sit amet'),
-                ],
-              )
-            ],
-          )),
+        appBar: AppBar(
+          title: Text("Appbar"),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              child: Text("Hai"),
+              decoration: BoxDecoration(color: Colors.red),
+            ),
+            Container(
+              child: Text("Hai"),
+              decoration: BoxDecoration(color: Colors.yellow),
+            ),
+            Container(
+              child: Text("Hai"),
+              decoration: BoxDecoration(color: Colors.green),
+            ),
+            Column(
+              children: <Widget>[
+                Text("Kolom"),
+                Text('Lorem ipsum dolor sit amet'),
+                TextButton(
+                  onPressed: () {
+                    print("Click TextButton");
+                  },
+                  child: Text("Button Text"),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    print("Click Outlined Button");
+                  },
+                  child: Text('Outlined Button'),
+                ),
+                IconButton(
+                  icon: Icon(Icons.volume_up),
+                  tooltip: 'Increase volume by 10',
+                  onPressed: () {
+                    print("Click Icon Button");
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
